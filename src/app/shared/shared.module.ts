45fb585727a39a2bcd58a347b03cdbd2bdf0c24f
Dariel Medina Rodriguez <darielmedr97@gmail.com>
2021-08-12 15:10:23 +0200

    feat: creates yt-player with iframe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MaterialModule } from './modules/material.module';
-import { YouTubePlayerModule } from '@angular/youtube-player';
-import { YtPlayerComponent } from './components/yt-player/yt-player.component';
 import { InterviewedCardComponent } from './components/interviewed-card/interviewed-card.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CoreModule } from '../core/core.module';
 import { ShowInViewDirective } from './directives/show-in-view.directive';
+import { YtPlayerComponent } from './components/yt-player/yt-player.component';
 
 const commonElementes: any[] = [
   YtPlayerComponent,
@@ -17,16 +16,15 @@ const commonElementes: any[] = [
 
 @NgModule({
   declarations: [
-    ...commonElementes,
+    ...commonElementes
   ],
   imports: [
     CommonModule,
     MaterialModule,
-    YouTubePlayerModule,
     CoreModule
   ],
   exports: [
     ...commonElementes
-  ]
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
